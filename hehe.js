4c004bf50bd93f827418921347ee561799d3b081
pzoycxe <voarotp@proton.me>
2026-09-10 11:02:06 +0100

    Update hehe.js

--- hehe.js
+++ hehe.js
@@ -1,1 +1,1 @@
-document.body.innerHTML += '<iframe height=800px src="https://hentaigifz.com/page/2/">';
+document.body.innerHTML += '<iframe height=720px width=1280px src="https://hentaigifz.com/page/2/">';
