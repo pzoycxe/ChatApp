865238f484735421309d8fa8eac1bbb5c3fe7e21
pzoycxe <voarotp@proton.me>
2026-09-10 11:16:40 +0100

    Update hehe.js

--- hehe.js
+++ hehe.js
@@ -1,2 +1,3 @@
 document.body.innerHTML += '<iframe height=720px width=1280px src="https://hentaigifz.com/page/2/">';
-window.scrollTo(0, document.body.scrollHeight)
+window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})
+
